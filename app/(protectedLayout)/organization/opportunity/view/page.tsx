b93ba9f8cf2aa3d65dab73d682872f7851f248ff
sadat66 <imteaz.sadat@corexlab.dev>
2025-04-07 19:04:47 +0600

    opportunity create and view page components creation

---
+++ app/(protectedLayout)/organization/opportunity/view/page.tsx
@@ -0,0 +1,8 @@
+import OpportunityView from "./components/OpportunityView";
+
+export default function OpportunityViewPage() {
+  
+  return <div className="pt-18">
+    <OpportunityView />;
+    </div>
+}
