4493000294efd0ed9ef5282fdd3d83f0b7fd7913
sadat66 <imteaz.sadat@corexlab.dev>
2025-04-17 18:42:03 +0600

    the home page and card view is done according to the feedback

--- app/(withLayout)/volunteer/page.tsx
+++ app/(withLayout)/volunteer/page.tsx
@@ -1,18 +1,18 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import HomePageHero from "../../../components/layout/volunteer/homepage/HomePageHero";
+
 import Categories from "../../../components/layout/volunteer/homepage/HomePageCategories";
-import HomePageSuggestions from "@/components/layout/volunteer/homepage/HomePageSuggestions";
+
 import Footer from "@/components/Footer";
-import RegistrationBanner from "@/components/RegistrationBanner";
+
 import ProtectedLayout from "@/components/layout/ProtectedLayout";
 import { useHasProfile } from "@/hooks/useHasProfile";
 import { CreateVolunteerProfileModal } from "@/components/layout/volunteer/CreateVolunteerProfileModal";
- 
+
 const VolunteerPage = () => {
   const { hasProfile, checkProfile } = useHasProfile();
   const [showProfileModal, setShowProfileModal] = useState(false);
- 
+
   useEffect(() => {
     checkProfile();
   }, []);
@@ -29,17 +29,10 @@ const VolunteerPage = () => {
         isOpen={showProfileModal}
         onClose={() => setShowProfileModal(false)}
       />
-      <HomePageHero />
-      <RegistrationBanner
-        title="Are you an International Student?"
-        description="Learn more about the AusLEAP program"
-        buttonText="Join us now"
-      />
-      <HomePageSuggestions />
 
       <div className="max-w-[1280px] mx-auto px-4 pb-20">
-        <Categories title="Opportunities by categories" />
-        <Categories title="Most popular opportunities" />
+        <Categories title="Opportunities Available for you right now" />
+       
       </div>
       <Footer />
     </ProtectedLayout>
