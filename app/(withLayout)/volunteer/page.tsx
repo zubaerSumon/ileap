436b86b0ec843891d901b6377d0f41fdd9c1b198
zubaer_sumon <zubaer.rahman.cse@gmail.com>
2025-05-08 12:44:24 +0600

    swiper removed to render all avaible cards

--- app/(withLayout)/volunteer/page.tsx
+++ app/(withLayout)/volunteer/page.tsx
@@ -9,14 +9,14 @@ const VolunteerPage = () => {
   return (
     <ProtectedLayout>
       <div className="px-4 flex flex-col items-center pb-40">
-        <h1 className="text-[#101010] font-inter text-2xl font-bold text-center mt-8 mb-2">
+        <h1 className="text-[#101010] font-inter text-3xl font-bold text-center mt-8 mb-2">
           Welcome 2025 AusLeap Participants
         </h1>
-        <p className="text-[#8D8D8D] text-sm font-inter text-m font-medium text-center mb-12">
+        <p className="text-[#8D8D8D] text-lg font-inter text-m font-medium text-center mb-12">
           We have some new volunteering opportunities available for you
         </p>
         {/* Update layout to flex-row for horizontal alignment on larger screens */}
-        <div className="flex flex-col md:flex-row justify-between space-y-5 items-center w-full">
+        <div className="flex flex-col md:flex-row justify-between space-y-5 w-full">
           <Categories />
           <MiniGallery />
         </div>
--- components/layout/volunteer/homepage/MiniGallery.tsx
+++ components/layout/volunteer/homepage/MiniGallery.tsx
@@ -28,51 +28,45 @@ const MiniGallery: React.FC<MiniGalleryProps> = ({
   return (
     <>
       {/* Desktop version */}
-      <div className="hidden sm:flex bg-[#254A9B] rounded-[51px] w-[380px] h-[400px] p-6 flex-col">
-        <h2 className="text-white text-center font-inter text-sm font-normal leading-normal mb-4">
-          {title}
-        </h2>
+      <div className="hidden sm:flex items-center  w-[340px] space-y-5 flex-col">
+        <h2 className="text-sm font-semibold  ">{title}</h2>
 
-        <div className="bg-[#F5F8FF] rounded-[40px]  p-5 flex-1 flex flex-col overflow-hidden">
-          <div className="flex flex-wrap gap-3 justify-center items-start">
-            {galleryImages.map((src, index) => (
-              <div
-                key={index}
-                className="relative rounded-xl overflow-hidden"
-                style={{
-                  height: imageHeight,
-                  width: `${parseInt(imageWidths[index]) * 0.9}px`,
-                 }}
-              >
-                <Image
-                  src={src}
-                  alt={`Gallery image ${index + 1}`}
-                  fill
-                  className="object-cover"
-                />
-              </div>
-            ))}
-          </div>
-
-          <div className="flex justify-center mt-auto mb-1">
-            <Link
-              href={seeMoreLink}
-              className="flex px-[18px] py-[6px] justify-center items-center gap-[6px] rounded-[6px] bg-[#2563EB] text-white text-sm font-medium hover:bg-blue-600 transition-colors w-[120px]"
+        <div className="flex flex-wrap gap-3 items-start justify-center">
+          {galleryImages.map((src, index) => (
+            <div
+              key={index}
+              className="relative rounded-xl overflow-hidden"
+              style={{
+                height: imageHeight,
+                width: `${parseInt(imageWidths[index]) * 0.9}px`,
+              }}
             >
-              See more
-            </Link>
-          </div>
+              <Image
+                src={src}
+                alt={`Gallery image ${index + 1}`}
+                fill
+                className="object-cover"
+              />
+            </div>
+          ))}
         </div>
+
+        <Link
+          href={seeMoreLink}
+          className="flex px-[18px] py-[6px] justify-center items-center gap-[6px] rounded-[6px] bg-[#2563EB] text-white text-sm font-medium hover:bg-blue-600 transition-colors w-[120px]"
+        >
+          See more
+        </Link>
       </div>
 
       {/* Mobile version */}
-      <div className="sm:hidden bg-[#254A9B] rounded-[51px] w-[340px] h-[400px] pb-3 pt-3 px-4 flex flex-col">
+      <div className="sm:hidden bg-[#254A9B] rounded-[51px]  pb-3 pt-3 px-4 flex flex-col">
         <h2 className="text-white text-center font-inter text-sm font-normal leading-normal pb-4 pt-2 px-2">
           {title}
         </h2>
 
         <div className="bg-[#F5F8FF] rounded-[51px] p-3 flex-1 flex flex-col">
-          <div className="flex flex-wrap gap-3 flex-1 justify-center items-start pt-3">
+          <div className="flex flex-wrap  gap-3 flex-1 justify-center items-start pt-3">
             {galleryImages
               .map((src, index) => (
                 <div
@@ -98,7 +92,7 @@ const MiniGallery: React.FC<MiniGalleryProps> = ({
               .slice(0, 3)}
           </div>
 
-          <div className="mt-3 flex justify-center w-[150px] mx-auto pb-4">
+          <div className=" flex mt-3 justify-center w-[150px] mx-auto pb-4">
             <Link
               href={seeMoreLink}
               className="flex px-[18px] py-[6px] justify-center items-center gap-[6px] flex-[1_0_0] rounded-[6px] bg-[#2563EB] text-white text-sm font-medium hover:bg-blue-600 transition-colors"
